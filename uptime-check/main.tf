92b4970ca4a8ee1d0b26af1e113eed1c93941579
Alex Coleman <alexjcoleman@gmail.com>
2024-06-08 15:52:26 +0100

    Add alert policy and notification channel
    First test using condition_absent check on uptime every 2mins

--- uptime-check/main.tf
+++ uptime-check/main.tf
@@ -25,3 +25,31 @@ resource "google_monitoring_uptime_check_config" "cloud_run" {
 
   depends_on = [google_project_service.monitoring]
 }
+
+resource "google_monitoring_alert_policy" "alert_policy" {
+  display_name = "Cloud run uptime alert"
+  project      = var.project
+  combiner     = "OR"
+  conditions {
+    display_name = "uptime check"
+    condition_absent {
+      filter   = format("metric.type=\"monitoring.googleapis.com/uptime_check/check_passed\" AND metric.label.check_id=\"%s\" AND resource.type=\"uptime_url\"", google_monitoring_uptime_check_config.cloud_run.uptime_check_id)
+      duration = "120s"
+      trigger {
+        count = 1
+      }
+    }
+  }
+  severity = "ERROR"
+  notification_channels = [google_monitoring_notification_channel.email.id]
+}
+
+resource "google_monitoring_notification_channel" "email" {
+  display_name = "Test Notification Channel"
+  project      = var.project
+  type         = "email"
+  labels = {
+    email_address = "canines_panacea_0u@icloud.com"
+  }
+  force_delete = false
+}
